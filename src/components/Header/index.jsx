abb35c0cded1a543da28557ecd9b740c23e02b09
pawankallem <kallem.pavan1997@gmail.com>
2023-10-05 09:51:47 +0530

    popup added to onchange event and showing suggestion and making api call on click

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import wikiLogo from "../../assets/wikiLogo.png"
+import React from "react";
+import wikiLogo from "../../assets/wikiLogo.png";
 
 function Header() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000';  //https://wiki-server-clone.onrender.com
+// Local: 'http://localhost:5000
+// Prod: 'https://wiki-server-clone.onrender.com
+axios.defaults.baseURL = "https://wiki-server-clone.onrender.com";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
